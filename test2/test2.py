827fed4cdbe6c7afef6169a8334db13868ec5f0b
Noha Mohamed <noha158@aucegypt.edu>
2020-05-27 02:53:37 +0200

    without input time

--- test2/test2.py
+++ test2/test2.py
@@ -8,39 +8,42 @@ import serial
 x_len = 200
 y_range = [0, 4096]
 
-def serialRead(samplingRate):
-    bpm = ser.readline().decode('utf-8')
-    while bpm.find('B') == -1:
-        bpm = ser.readline().decode('utf-8')
-        bpm = bpm.strip("\n")
-        bpm = bpm.strip("\r")
-    return bpm
-# This function is called periodically from FuncAnimation
-
 def inti_func():
     plt.title('Heart Rate Monitor')
     plt.xlabel('Samples')
     plt.ylabel('ECG Output')
 
-def animate(i,ys,samplingRate):
+def readData(samplingRate):
     global bpmFinal
-    print("in animate")
-    line = ser.readline().decode('utf-8')
-    line = line.strip("\n")
-    line = line.strip("\r")
-    print("val = ", str(line))
-    if(line.find('B') != -1):
-        print("time ended")
+    ecgList = []
+    stopFlag = False
+    samplingRate = int(samplingRate)
+    looper = int(0.25*samplingRate)
+    for i in range(looper):
+        line = ser.readline().decode('utf-8')
+        line = line.strip("\n")
+        line = line.strip("\r")
+        if(line.find('B') != -1):
+            stopFlag = True
+            bpmFinal = line
+        if ((len(line) > 0) and (len(line) < 5) and (line.find('\r') == -1)):
+            line = float(line)
+            ecgList.append(line)
+            #print("val = ", str(line))
+    return ecgList, stopFlag
+
+
+def animate(i,ys,samplingRate):
+    ecgList2, stopFlag2 = readData(samplingRate)
+    if(stopFlag2 == True):
         ys.clear()
         ys = [0] * x_len
-        bpmFinal = line
         ani.event_source.stop()
-    if((len(line) > 0) and (len(line) < 5) and (line.find('\r')==-1)):
-        line = float(line)
-        ys.append(line)
-        ys = ys[-x_len:]
+    for x in ecgList2:
+        ys.append(x)
+    ys = ys[-x_len:]
 
-        line2.set_ydata(ys)
+    line2.set_ydata(ys)
 
     return line2,
 
@@ -55,16 +58,15 @@ if __name__ == '__main__':
     baudrate = input("Enter the baud rate: ")
     ser.port = port
     ser.baudrate = baudrate
-    samplingRate = input("Enter the Sampling rate: ")
     if ser.is_open is not True:
         ser.open()
     bpm = ""
     call = 0
     while True:
         command = input("Enter your command: ")
-        samplingRate2 = samplingRate + "\r"
         if(command == "start"):
-            print("in")
+            samplingRate = input("Enter the Sampling rate: ")
+            samplingRate2 = samplingRate + "\r"
             call = call + 1
             ser.write(samplingRate2.encode())
             fig = plt.figure()
@@ -73,8 +75,7 @@ if __name__ == '__main__':
             ys = [0] * x_len
             ax.set_ylim(y_range)
             line2, = ax.plot(xs, ys)
-            ani = animation.FuncAnimation(fig, animate, init_func=inti_func, repeat=True, fargs=(ys, samplingRate), interval=1, save_count=200)
-            print("why")
+            ani = animation.FuncAnimation(fig, animate, init_func=inti_func, repeat=True, fargs=(ys, samplingRate), interval=100, save_count=200)
             plt.show()
         elif (command == "bpm"):
             print(bpmFinal)
